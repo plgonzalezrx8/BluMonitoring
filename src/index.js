3c4a7cb330f7613c1b4b01c6b60736a0464c8951
Pedro Gonzalez <plgonzalez@protonmail.com>
2020-09-25 21:53:29 -0400

    Updated window size.

--- src/index.js
+++ src/index.js
@@ -11,13 +11,25 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    maxHeight: 600,
+    minHeight: 400,
+    maxWidth: 800,
+    minWidth: 600,
+
   });
+
+  //options for mainWindow
+  mainWindow.setMenuBarVisibility(false);
+
+
+
+
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
