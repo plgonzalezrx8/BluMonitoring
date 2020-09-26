480b6b793d6407b3f76017fad0b58ff6c9e9733a
Pedro Gonzalez <plgonzalez@protonmail.com>
2020-09-25 22:01:08 -0400

    Added new CSS rules to avoid interacting with HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+//require('electron-disable-file-drop');
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
